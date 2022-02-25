44d4deb4d14e74780e75cb2d48dd00c341b9e690
alex-gre <alexstv@mail.ru>
2022-02-25 19:19:55 +0300

    Подключение к базе и реализация функций select, insert, delete в файле organizerdb.py

---
+++ SS/organizerdb.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+def input_contacts_base():
+	# connect database
+    conn = sqlite3.connect("organizer.db")
+    cursor = conn.cursor()
+	#create table and save schema
+    cursor.execute('CREATE TABLE IF NOT EXISTS phone_book(id INTEGER PRIMARY KEY AUTOINCREMENT,\
+         name VARCHAR(35) NOT NULL, n_phone VARCHAR(14) NOT NULL, info VARCHAR(125))')
+    conn.commit()
+
+    #input data console
+    name = input('Введите ФИО: ')
+    n_phone = input('Введите номер телефона: ')
+    info = input('Доп информация 125 символов: ')
+
+
+    cursor.execute('INSERT INTO phone_book (name,n_phone,info) VALUES (?,?,?)',(name,n_phone,info))
+    #зафиксировать транзакцию
+    conn.commit()
+    #закрыть курсор
+    cursor.close()
+    #закрыть подключение
+    conn.close()
+    print("Insert Table Ok")
+
+def view_table():
+    conn = sqlite3.connect("organizer.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM phone_book")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row) # it print all records in the database
+
+    conn.close()
+
+def clear_table():
+    conn = sqlite3.connect("organizer.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM phone_book")
+  
+    #зафиксировать транзакцию
+    conn.commit()
+    #закрыть курсор
+    cursor.close() 
+    conn.close()
+        
+    print("Clear Table Ok")
+        
