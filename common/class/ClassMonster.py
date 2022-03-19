9578b0fff3579bf20fd540d95de9549f148db84b
alex-gre <alexstv@mail.ru>
2022-03-19 10:09:30 +0300

    добавлена папка class для изучения классов ооп

---
+++ common/class/ClassMonster.py
@@ -0,0 +1,18 @@
+
+class ClassMonster:
+    def __init__(self, x,y):# конструктор класса
+        print('Monster initialize...')
+        self.x_coordinate = x   # поля
+        self.y_coordinate = y
+
+    def moveme(self, new_x,new_y):
+        print('Moveme to ...')
+        self.x_coordinate = self.x_coordinate + new_x
+        self.y_coordinate = self.y_coordinate + new_y
+
+
+zombie = ClassMonster(24,45)
+print(f'Coordinate x = {zombie.x_coordinate} y = {zombie.y_coordinate}')
+zombie.moveme(36,20)
+print(f'x = {zombie.x_coordinate} y = {zombie.y_coordinate}')
+
